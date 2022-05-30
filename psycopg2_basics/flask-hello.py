c534fe536c947e2dbc7df29a85d352bb2e7777ee
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2022-05-30 14:34:30 +0100

    sqlAlchemyORM
    - created a database with sqlAlchemyORM

--- psycopg2_basics/flask-hello.py
+++ psycopg2_basics/flask-hello.py
@@ -1,8 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote_plus as urlquote
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:%s@127.0.0.1:5432/test' % urlquote(
+    'theceo@16')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+
+db.create_all()
 
 
 @app.route('/')
 def index():
     return ("Hello Flask, What's Up!")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
