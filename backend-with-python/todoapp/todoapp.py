3005a726b4712626aefef5f4053eaf949422e8ae
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2022-05-31 13:59:44 +0100

    restrurced files

---
+++ backend-with-python/todoapp/todoapp.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from urllib.parse import quote_plus as urlquote
+
+from sqlalchemy import PrimaryKeyConstraint
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:%s@localhost:5432/todoapp' % urlquote(
+    'theceo@16')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __repr__(self) -> str:
+        return f'<Todo ID: {self.id} Description: {self.description}>'
+
+
+@app.route('/')
+def index():
+    return ("Hello")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
